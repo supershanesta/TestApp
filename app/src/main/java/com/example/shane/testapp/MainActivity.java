e1f8bfc6e12e6f60d88af2cea5ace538efb01eff
shane <shanerice17@gmail.com>
2016-10-12 15:41:03 -0700

    second

---
+++ app/src/main/java/com/example/shane/testapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.shane.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static com.example.shane.testapp.R.id.editText1;
+import static com.example.shane.testapp.R.id.textView2;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    public void checkthisout(View v) {
+        EditText editText = (EditText) findViewById(R.id.editText1);
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText("");
+        textView.append(editText.getText());
+
+
+    }
+
+}
+
+
+
